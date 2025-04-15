4c08f6709f599d613df9a01c4f91750431ca500b
Sian Shinjo <sianshinjo123@gmail.com>
2025-04-15 11:28:08 +0530

    half of q3 done

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+// capturing events
 function record(event, object) {
     const tstamp = new Date().toString();
     console.log("TIMESTAMP: "+tstamp+", EVENT: "+event+", OBJECT: "+object);
@@ -12,7 +13,7 @@ function ifclicked(event) {
         object = "link";
     } else if (tag === "div") {
         object = "div";
-    } else if (["p", "h1", "h2", "h3", "h4", "h5", "h6", "ul", "dl", "ol", "li", "dt", "dd", "i"].includes(tag)) {
+    } else if (["p", "h1", "h2", "h3", "h4", "h5", "h6", "ul", "dl", "ol", "li", "dt", "dd", "i", "b"].includes(tag)) {
         object = "text";
     } else if (tag === "header") {
         object = "header";
@@ -28,6 +29,52 @@ function ifclicked(event) {
 
 document.addEventListener("DOMContentLoaded", () => {
     record("view", "page");
-})
+    let userinput = document.getElementById("userinput");
+    let submitted = document.getElementById("submitfb");
+    
+    if (userinput) {
+        userinput.addEventListener("input", () => {
+            document.getElementById("inputwarning").style.display = "none";
+            processing();
+        });
+    }
+
+    if (submitted) {
+        submitted.addEventListener("click", () => {
+            validate_feedback();
+        });
+    }
+});
 
 document.addEventListener("click", ifclicked);
+
+
+// feedback section
+function validate_feedback() {
+    const text = document.getElementById("userinput").value;
+    if (text.length <= 10000) {
+        document.getElementById("inputwarning").style.display = "block";
+    } else {
+        document.getElementById("inputwarning").style.display = "none";
+    }
+}
+
+function processing() {
+    let text = document.getElementById("userinput").value;
+
+    let letters = (text.match(/[a-zA-Z]/g) || "").length;
+    let words = (text.split(/\s+/g).filter(Boolean) || "").length;
+    let spaces = (text.match(/\s/g) || "").length;
+    let newlines = (text.match(/\n/g) || "").length;
+    let specials = (text.match(/[^a-zA-Z0-9\s\n]/g) || "").length;
+
+    if (text === "") {
+        letters = words = spaces = newlines = specials = 0;
+    }
+
+    document.getElementById("letters").textContent = letters;
+    document.getElementById("words").textContent = words;
+    document.getElementById("spaces").textContent = spaces;
+    document.getElementById("newlines").textContent = newlines;
+    document.getElementById("specials").textContent = specials;
+}
